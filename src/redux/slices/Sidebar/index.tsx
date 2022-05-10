67bb1df8d126aa943290915e017f6ac48baff4ba
Henry Gabriel <henry.rezende@aquare.la>
2022-05-10 19:01:07 -0300

    feat: created sidebar slice

---
+++ src/redux/slices/Sidebar/index.tsx
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+interface SidebarState {
+  open: boolean;
+}
+
+const initialState = { open: false } as SidebarState;
+
+export const sidebarSlice = createSlice({
+  name: 'sidebar',
+  initialState,
+  reducers: {
+    changeSidebarState(state) {
+      state.open = !state.open;
+    },
+  },
+});
+
+export const { changeSidebarState } = sidebarSlice.actions;
+
+export default sidebarSlice.reducer;
