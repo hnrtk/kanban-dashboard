9d4dea812612ce962a8c9b6ce44ebde12c2de779
Henry Gabriel <henry.rezende@aquare.la>
2022-05-16 19:30:12 -0300

    feat: nav using keyboard

--- src/components/Sidebar/TopSidebar/Item.tsx
+++ src/components/Sidebar/TopSidebar/Item.tsx
@@ -10,39 +10,47 @@ interface Props {
 
 export function TopSideItem({ open, name, icon, route }: Props) {
   return open ? (
-    <Stack
-      direction='row'
-      alignItems='center'
-      w='100%'
-      bgColor='blue.700'
-      borderRadius='12px'
-      py='6px'
-      px='12px'
-      cursor='pointer'
+    <ChakraUILink
+      as={Link}
+      to={route}
+      display='flex'
+      textDecoration='none'
       _hover={{
-        backgroundColor: 'blue.800',
-        transition: 'all 0.4s',
+        textDecoration: 'none',
       }}
     >
-      <ChakraUILink as={Link} to={route}>
+      <Stack
+        direction='row'
+        alignItems='center'
+        w='100%'
+        bgColor='blue.700'
+        borderRadius='12px'
+        py='6px'
+        px='12px'
+        cursor='pointer'
+        _hover={{
+          backgroundColor: 'blue.800',
+          transition: 'all 0.4s',
+        }}
+      >
         {icon}
         <Text whiteSpace='nowrap'>{name}</Text>
-      </ChakraUILink>
-    </Stack>
+      </Stack>
+    </ChakraUILink>
   ) : (
     <Tooltip label={name} hasArrow placement='right' aria-label={name} gutter={24}>
-      <ChakraUILink as={Link} to={route}>
-        <IconButton
-          aria-label={name}
-          icon={icon}
-          background='none'
-          borderRadius='8px'
-          _hover={{
-            background: 'blue.500',
-            transform: 'scale(1.2)',
-          }}
-        />
-      </ChakraUILink>
+      <IconButton
+        as={Link}
+        to={route}
+        aria-label={name}
+        icon={icon}
+        background='none'
+        borderRadius='8px'
+        _hover={{
+          background: 'blue.500',
+          transform: 'scale(1.2)',
+        }}
+      />
     </Tooltip>
   );
 }
--- src/components/Sidebar/TopSidebar/TopSidebarOpen.tsx
+++ src/components/Sidebar/TopSidebar/TopSidebarOpen.tsx
@@ -1,4 +1,4 @@
-import { Flex, HStack, Stack, Text, VStack } from '@chakra-ui/react';
+import { Flex, HStack, Stack, Text } from '@chakra-ui/react';
 import { AngularLogo, Clock, Kanban, Newspaper, SquaresFour } from 'phosphor-react';
 import { SidebarTrigger } from '../Trigger';
 import { TopSideItem } from './Item';
@@ -16,21 +16,19 @@ const items = [
 
 export function TopSideOpen(props: Props) {
   return (
-    <Flex flexDir='column' w='100%'>
-      <Stack direction='column' spacing={6}>
-        <Flex justifyContent='space-between' alignItems='center' w='100%'>
-          <HStack spacing={2} h='100%'>
-            <AngularLogo size='32px' color='#f10' />
-            <Text whiteSpace='nowrap'>Nome do projeto</Text>
-          </HStack>
-          <SidebarTrigger {...props} />
-        </Flex>
-        <Stack direction='column' as='nav' w='100%' spacing={4}>
-          {items.map((item) => (
-            <TopSideItem key={item.name} open={props.open} {...item} />
-          ))}
-        </Stack>
+    <Stack direction='column' spacing={6} w='100%'>
+      <Flex justifyContent='space-between' alignItems='center' w='100%'>
+        <HStack spacing={2} h='100%'>
+          <AngularLogo size='32px' color='#f10' />
+          <Text whiteSpace='nowrap'>Nome do projeto</Text>
+        </HStack>
+        <SidebarTrigger {...props} />
+      </Flex>
+      <Stack direction='column' as='nav' w='100%' spacing={4}>
+        {items.map((item) => (
+          <TopSideItem key={item.name} open={props.open} {...item} />
+        ))}
       </Stack>
-    </Flex>
+    </Stack>
   );
 }
